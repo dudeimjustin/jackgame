b81087824b4513125c3d14b7185f0a59a39ad5b2
dudeimjustin <guitarplayer1291@hotmail.com>
2014-05-11 17:26:25 -0700

    Keyboard

---
+++ keyboard.py
@@ -0,0 +1,48 @@
+import pygame
+
+pygame.init()
+
+window = pygame.display.set_mode((800,600))
+
+pygame.display.set_caption("Window")
+
+black = (0,0,0)
+
+white = (255,255,255)
+
+x,y=0,0
+
+gameLoop=True
+while gameLoop:
+
+    for event in pygame.event.get():
+
+        if (event.type==pygame.QUIT):
+
+            gameLoop=False
+
+        if (event.type==pygame.KEYDOWN):
+
+            if (event.type==pygame.K_LEFT):
+
+                x-=10
+
+            if (event.type==pygame.K_RIGHT):
+
+                x+=10
+
+            if (event.type==pygame.K_UP):
+
+                y-=10
+
+            if (event.type==pygame.K_DOWN):
+
+                y+=10
+
+    window.fill(black)
+
+    pygame.draw.rect(window,white,(x,y,50,50))
+
+    pygame.display.flip()
+
+pygame.quit()
